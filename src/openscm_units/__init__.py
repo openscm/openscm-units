193349ede344fd7aff39a0c64eb55a6ae604ffc0
Mika Pflüger <mika.pflueger@pik-potsdam.de>
2020-12-18 10:31:18 +0100

    Lift ScmUnitRegistry to main module.

--- src/openscm_units/__init__.py
+++ src/openscm_units/__init__.py
@@ -3,6 +3,7 @@ OpenSCM Units, units for use with simple climate modelling
 """
 from ._version import get_versions
 from .unit_registry import unit_registry  # noqa: F401
+from .unit_registry import ScmUnitRegistry
 
 __version__ = get_versions()["version"]
 del get_versions
