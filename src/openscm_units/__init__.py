e596fe3b915513bd69a80af164f45377ec519441
Mika Pflüger <mika.pflueger@pik-potsdam.de>
2020-12-18 11:25:05 +0100

    Ignore F401 (unused import).

--- src/openscm_units/__init__.py
+++ src/openscm_units/__init__.py
@@ -1,7 +1,7 @@
 """
 OpenSCM Units, units for use with simple climate modelling
 """
-from ._unit_registry import ScmUnitRegistry, unit_registry
+from ._unit_registry import ScmUnitRegistry, unit_registry  # noqa: F401
 from ._version import get_versions
 
 __version__ = get_versions()["version"]
