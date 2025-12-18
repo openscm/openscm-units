a14c224f2a6d84b535946172c0d3547048a4ad1f
Jared Lewis <jared@jared.kiwi.nz>
2025-12-18 12:36:45 +1100

    chore: Update ruff errors

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ Re-useable fixtures etc. for tests
 
 See https://docs.pytest.org/en/7.1.x/reference/fixtures.html#conftest-py-sharing-fixtures-across-multiple-files
 """
+
 from pathlib import Path
 
 import pytest
